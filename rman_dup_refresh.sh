d13e3475ab6fb32b00650497c072b72411ec1879
Josh Denize <33252865+denizej@users.noreply.github.com>
2019-02-08 14:42:10 +0000

    updates made at HCUK
    updates made at HCUK, particularly how command line options are specified

--- rman_dup_refresh.sh
+++ rman_dup_refresh.sh
@@ -1,32 +1,95 @@
 #!/bin/bash
 
-USAGE="
-USAGE: `basename $0` <SID_PREFIX> <SOURCE_ENV> <DEST_ENV> [FUNCTION_TO_EXEC]\n
-EG: - `basename $0` josh c1 c2\n
-OR: - `basename $0` josh c2 c3\n
-OR: - `basename $0` josh c2 c3 list_all_functions\n
-"
+# set defaults
+RETURN_STATUS=0
+DEBUG_LEVEL=0
+NO_PROMPT=0
 
+function usage()
+{
+  echo
+  echo "Usage: $(basename $1) [-help|-h] [-debug] [-noPromopt] {-system=SYSTEM} {-source_env=SOURCE_ENV} {-dest_env=DEST_ENV} [-func=FUNCTION_TO_EXEC]"
+  echo "Where:"
+  echo "  -help|-h         - Optional: Show this usage"
+  echo "  -debug           - Optional: (default is off)"
+  echo "  -noPrompt        - Optional: Setting this option will run with no prompts to the user (default is to prompt if an interactive session)"
+  echo "  -system          - Required: Name of the system that forms the common prefix of the database SID"
+  echo "  -source_env      - Required: Name of the source environment that forms the suffix of the database SID"
+  echo "  -dest_env        - Required: Name of the destination environment that forms the suffix of the database SID"
+  echo "  -func            - Optional: Name of a function to execute"
+} # function usage
 
-if [ $# -lt 3 ]; then
-  echo "ERROR: incorrect usage"
-  echo -e $USAGE
-  #echo "  USAGE: `basename $0` <SYSTEM> <SOURCE_ENV> <DEST_ENV> [FUNCTION_TO_EXEC]"
-  #echo "example: `basename $0` josh c1 c2"
+# check inputs
+if [ $# -eq 0 ]; then
+  echo -e "\nERROR: incorrect usage"
+  usage $0
   exit 1
 fi
 
-SCRIPT_NAME=`basename $0|cut -d. -f1`
+# read inputs
+while [[ $# -gt 0 ]]
+do
+  param=$(echo $1|cut -d= -f1)
+  value=$(echo $1|cut -d= -f2)
+  #echo param = $param
+  #echo value = $value
+  case $param in
+        -help|-h)
+                usage $0
+                exit
+                ;;
+        -debug)
+                #DEBUG_LEVEL="$value"
+                DEBUG_LEVEL=1
+                ;;
+        -noPrompt)
+                NO_PROMPT=1
+                ;;
+        -system)
+                #SYSTEM="$value"
+                SYSTEM=$(echo $value | tr '[:lower:]' '[:upper:]')
+                ;;
+        -source_env)
+                SOURCE_ENV=$(echo $value | tr '[:lower:]' '[:upper:]')
+                ;;
+        -dest_env)
+                DEST_ENV=$(echo $value | tr '[:lower:]' '[:upper:]')
+                ;;
+        -func)
+                FUNCTION_TO_EXEC="$value"
+                ;;
+        *)
+                echo -e "\nERROR: Invalid parameter: $param"
+                usage $0
+                exit 1
+                ;;
+  esac
+  shift
+done
 
-SYSTEM=`echo $1 | tr '[:lower:]' '[:upper:]'`
-SOURCE_ENV=`echo $2 | tr '[:lower:]' '[:upper:]'`
-DEST_ENV=`echo $3 | tr '[:lower:]' '[:upper:]'`
+# check required inputs
+if [ -z $SYSTEM ]; then
+  echo -e "\nERROR: -system required"
+  usage $0
+  exit 1
+fi
+if [ -z $SOURCE_ENV ]; then
+  echo -e "\nERROR: -source_env required"
+  usage $0
+  exit 1
+fi
+if [ -z $DEST_ENV ]; then
+  echo -e "\nERROR: -dest_env required"
+  usage $0
+  exit 1
+fi
 
-FUNCTION_TO_EXEC=$4
 
-THIS_DIR=`pwd`
-cd `dirname $0`
-BASE_DIR=`pwd`
+SCRIPT_NAME=$(basename $0|cut -d. -f1)
+
+THIS_DIR=$(pwd)
+cd $(dirname $0)
+BASE_DIR=$(pwd)
 cd $THIS_DIR
 
 ENV_FILE=$BASE_DIR/$SCRIPT_NAME.env
@@ -37,8 +100,8 @@ export SYS_DIR=$BASE_DIR/$SYSTEM
 LOGS_DIR=$SYS_DIR/logs
 
 if [ ! -r $ENV_FILE ]; then
-  echo ERROR: cannot read environment file $ENV_FILE
-  echo -e $USAGE
+  echo -e "\nERROR: cannot read environment file $ENV_FILE"
+  usage $0
   exit 1
 else
   # source in the main environment file
@@ -46,8 +109,8 @@ else
 fi
 
 if [ ! -r $FUNC_FILE ]; then
-  echo "ERROR: cannot read function file $FUNC_FILE"
-  echo -e $USAGE
+  echo -e "\nERROR: cannot read function file $FUNC_FILE"
+  usage $0
   exit 1
 else
   # source in the function file
@@ -57,57 +120,48 @@ fi
 if [ ! -d $LOGS_DIR ]; then
   mkdir -p $LOGS_DIR
   if [ ! $? -eq 0 ]; then
-    echo ERROR: could not create directory $LOGS_DIR
+    echo -e "\nERROR: could not create directory $LOGS_DIR"
     exit 1
   fi
 fi
 
 if [ ! -d $SYS_DIR ]; then
-  echo "ERROR: cannot access directory $SYS_DIR"
-  echo -e $USAGE
+  echo -e "\nERROR: cannot access directory $SYS_DIR"
   exit 1
 elif [ ! -d $SYS_DIR/backups ]; then
   mkdir -p $SYS_DIR/backups
   if [ ! $? -eq 0 ]; then
-    echo "ERROR: could not create directory $SYS_DIR/backups"
+    echo -e "\nERROR: could not create directory $SYS_DIR/backups"
     exit 1
   fi
 fi
 
-COUNT=`grep -c ^'function '${FUNCTION_TO_EXEC}'()' $FUNC_FILE`
-if   [ $# -eq 4 ] && [ $COUNT -gt 1 ]; then
-  echo -e $USAGE
-  echo -e "\nERROR: please be more specific as $FUNCTION_TO_EXEC matched $COUNT possible functions"
-  echo -e "\nThe list of functions are:\n"
-  grep ^function $FUNC_FILE |awk {'print$2'}|cut -d\( -f1|sort|grep 'DB'
-  echo
-  exit 1
-elif [ $# -eq 4 ] && [ $COUNT -eq 0 ]; then
-  echo -e $USAGE
+#COUNT=$(grep -c ^'function '${FUNCTION_TO_EXEC}'()' $FUNC_FILE)
+COUNT=$(grep -c ^'function '${FUNCTION_TO_EXEC}'(){'  $FUNC_FILE)
+if [ ! -z $FUNCTION_TO_EXEC ] && [ $COUNT -eq 0 ]; then
+  usage $0
   echo -e "\nERROR: no function found called $FUNCTION_TO_EXEC"
-  echo -e "\nThe list of functions are:\n"
+  echo -e "\nThe list of functions available to be run are:\n"
   grep ^function $FUNC_FILE |awk {'print$2'}|cut -d\( -f1|sort|grep 'DB'
   echo
   exit 1
-elif [ $# -eq 3 ]; then 
-  unset FUNCTION_TO_EXEC
 fi
 
 if [ ! -d $POST_DB_REFRESH_SCRIPT_DIR ]; then
-  echo "ERROR: cannot access directory $POST_DB_REFRESH_SCRIPT_DIR"
-  echo -e $USAGE
+  echo -e "\nERROR: cannot access directory $POST_DB_REFRESH_SCRIPT_DIR"
+  usage $0
   exit 1
 fi
 
 if [ ! -r $POST_DB_REFRESH_SCRIPT_DEF_FILE ]; then
-  echo "ERROR: cannot read file $POST_DB_REFRESH_SCRIPT_DEF_FILE"
-  echo -e $USAGE
+  echo -e "\nERROR: cannot read file $POST_DB_REFRESH_SCRIPT_DEF_FILE"
+  usage $0
   exit 1
 fi
 
 if [ ! -r $POST_DB_REFRESH_SCRIPT_SQL_FILE ]; then
-  echo "ERROR: cannot read file $POST_DB_REFRESH_SCRIPT_SQL_FILE"
-  echo -e $USAGE
+  echo -e "\nERROR: cannot read file $POST_DB_REFRESH_SCRIPT_SQL_FILE"
+  usage $0
   exit 1
 fi
 
@@ -115,8 +169,8 @@ fi
 #MAIN_LOG_FILE=$LOGS_DIR/${SCRIPT_NAME}_${SOURCE_SID}_${DEST_SID}_${DATETIME}.log
 touch $MAIN_LOG_FILE
 if [ ! -r $MAIN_LOG_FILE ]; then
-  echo "ERROR: cannot write file $MAIN_LOG_FILE"
-  echo -e $USAGE
+  echo -e "\nERROR: cannot write file $MAIN_LOG_FILE"
+  usage $0
   exit 1
 fi
 
@@ -142,8 +196,6 @@ echo
 check_if_interactive
 #echo INTERACTIVE = $INTERACTIVE
 
-RETURN_STATUS=0
-
 
 
 # check if a single fuinction is to be executed only
@@ -156,11 +208,11 @@ fi
 
 
 # start_oem_blackouts_DEST_DB
-echo -e "\nNext: start OEM Cloud Control blackouts for database $DEST_SID and its listener"
-NEXT_STEP=start_oem_blackouts_DEST_DB
-prompt_if_interactive $NEXT_STEP
+#echo -e "\nNext: start OEM Cloud Control blackouts for database $DEST_SID and its listener"
+#NEXT_STEP=start_oem_blackouts_DEST_DB
+#prompt_if_interactive $NEXT_STEP
 #$NEXT_STEP
-log_return_status_pass_warning
+#log_return_status_pass_warning
 
 
 # tnsping_SOURCE_DB
@@ -270,9 +322,14 @@ log_return_status_pass_fail
 check_return_status_pass_fail
 
 
+
 # do_rman_dup_from_active_to_DEST_DB
 echo -e "\nNext: start the RMAN duplication from $SOURCE_SID to recreate $DEST_SID"
-NEXT_STEP=do_rman_dup_from_active_to_DEST_DB
+if [ $ORAVER == "12c" ]; then
+  NEXT_STEP=do_rman_dup_from_active_to_DEST_DB_12c
+else
+  NEXT_STEP=do_rman_dup_from_active_to_DEST_DB
+fi
 prompt_if_interactive $NEXT_STEP
 log_return_status_pass_fail
 check_return_status_pass_fail
@@ -288,11 +345,11 @@ log_return_status_pass_warning
 
 
 # disable_auto_maint_tasks_DEST_DB
-echo -e "\nNext: disable the automated maintenance tasks in $DEST_SID" 
-NEXT_STEP=disable_auto_maint_tasks_DEST_DB
-prompt_if_interactive $NEXT_STEP 
+#echo -e "\nNext: disable the automated maintenance tasks in $DEST_SID" 
+#NEXT_STEP=disable_auto_maint_tasks_DEST_DB
+#prompt_if_interactive $NEXT_STEP 
 #$NEXT_STEP 
-log_return_status_pass_warning
+#log_return_status_pass_warning
 
 
 # disable_archive_log_mode_DEST_DB
@@ -342,7 +399,6 @@ NEXT_STEP=cleanup_unified_auditing_DEST_DB
 prompt_if_interactive $NEXT_STEP
 #$NEXT_STEP
 log_return_status_pass_warning
-
 fi
 
 
@@ -363,17 +419,17 @@ log_return_status_pass_warning
 
 
 # disable_all_non_system_scheduler_jobs_DEST_DB
-echo -e "\nNext: disable all non system DBMS Scheduler jobs in $DEST_SID database"
-NEXT_STEP=disable_all_non_system_scheduler_jobs_DEST_DB
-prompt_if_interactive $NEXT_STEP
-log_return_status_pass_warning
+#echo -e "\nNext: disable all non system DBMS Scheduler jobs in $DEST_SID database"
+#NEXT_STEP=disable_all_non_system_scheduler_jobs_DEST_DB
+#prompt_if_interactive $NEXT_STEP
+#log_return_status_pass_warning
 
 
 # break_all_non_system_db_jobs_DEST_DB
-echo -e "\nNext: break all non system database jobs in $DEST_SID database"
-NEXT_STEP=break_all_non_system_db_jobs_DEST_DB
-prompt_if_interactive $NEXT_STEP
-log_return_status_pass_warning
+#echo -e "\nNext: break all non system database jobs in $DEST_SID database"
+#NEXT_STEP=break_all_non_system_db_jobs_DEST_DB
+#prompt_if_interactive $NEXT_STEP
+#log_return_status_pass_warning
 
 
 # execute_post_DB_refresh_script
@@ -383,12 +439,19 @@ prompt_if_interactive $NEXT_STEP
 log_return_status_pass_warning
 
 
+# do_rman_full_backup_DEST_DB
+#echo -e "\nNext: perform a full cold backup of the $DEST_SID database"
+#NEXT_STEP=do_rman_full_backup_DEST_DB
+#prompt_if_interactive $NEXT_STEP
+#log_return_status_pass_warning
+
+
 # stop_oem_blackouts_DEST_DB
-echo -e "\nNext: stop OEM Cloud Control blackouts for database $DEST_SID and its listener"
-NEXT_STEP=stop_oem_blackouts_DEST_DB
-prompt_if_interactive $NEXT_STEP
+#echo -e "\nNext: stop OEM Cloud Control blackouts for database $DEST_SID and its listener"
+#NEXT_STEP=stop_oem_blackouts_DEST_DB
+#prompt_if_interactive $NEXT_STEP
 #$NEXT_STEP
-log_return_status_pass_warning
+#log_return_status_pass_warning
 
 
 echo
